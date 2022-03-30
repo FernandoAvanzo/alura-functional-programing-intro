aff0ad7dfba485e7091a07b6328813a40fc637ab
fernando.avanzo <fernando.avanzo@gmail.com>
2022-03-30 17:04:51 -0300

    feat:alura-into-func-prg:lesson-01

---
+++ src/main/clojure/course/lesson01.clj
@@ -0,0 +1,17 @@
+;That function just print a message in the screen
+(defn   my-print-message
+  "That function just print a message in the screen"
+  []
+  (println "Hellow World"))
+
+;That function apply 10% of decount in the parameter
+(defn minus-ten-percent-of
+  "subtract of the value 10% of itselt"
+  [value]
+  (- value (ten-percent-of value)))
+
+;Is a good practice in clojure define declartives names in functions
+(defn ten-percent-of
+  "return 10% value of the paramenter"
+  [value]
+  (* value 0.10))
