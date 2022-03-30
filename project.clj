7b40356cb5d2bf00b7688f6741bdb219f7eadf05
fernando.avanzo <fernando.avanzo@gmail.com>
2022-03-30 15:33:33 -0300

    feat:alura-into-func-prg:setup project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject alura-functional-programing-intro "1.0.0-SNAPSHOT"
+  :description "Course sample code"
+  :url "https://github.com/FernandoAvanzo/alura-functional-programing-intro"
+  :dependencies [[org.clojure/clojure "1.11.0"]]
+  :plugins [[lein-tar "3.3.0"]]
+  :source-paths ["src" "src/main/clojure"]
+  :test-paths ["test" "test/main/clojure"]
+  :resources-paths ["resources"]
+  )
