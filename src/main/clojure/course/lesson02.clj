861483035a89e4b9a60fdc8f77e2191657c1946c
fernando.avanzo <fernando.avanzo@gmail.com>
2022-03-31 15:58:54 -0300

    feat:alura-into-func-prg:lesson-02

--- src/main/clojure/course/lesson02.clj
+++ src/main/clojure/course/lesson02.clj
@@ -8,3 +8,22 @@
     )
   )
 
+;Function copy paste from the couser v2
+(defn valor-descontado
+  "Retorna o valor com desconto de 10%."
+  [valor-bruto]
+  (let [taxa-de-desconto (/ 10 100)
+        desconto (* valor-bruto taxa-de-desconto)]
+    (println "Calculando desconto de" desconto)
+    (- valor-bruto desconto)))
+
+;Function with if operator version final
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (> valor-bruto 100)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (println "Calculando desconto de" desconto)
+      (- valor-bruto desconto))
+    valor-bruto))
