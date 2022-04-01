3f7cf379f3548b8cdea9defc1c3b16cda2d7681c
fernando.avanzo <fernando.avanzo@gmail.com>
2022-04-01 09:54:04 -0300

    feat:alura-into-func-prg: create predicate apply descout

--- src/main/clojure/course/lesson03.clj
+++ src/main/clojure/course/lesson03.clj
@@ -1,2 +1,25 @@
 (ns course.lesson03)
-(println "Bem vindo ao sistema de estoque")
+
+; PREDICATE
+(defn aplica-desconto?
+  [valor-bruto]
+  (if (> valor-bruto 100)
+    true
+    false))
+
+(println (aplica-desconto? 000.00))
+
+(println (aplica-desconto? 100.00))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (> valor-bruto 100)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto         (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado 1000.00))
+
+(println (valor-descontado 100.00))
