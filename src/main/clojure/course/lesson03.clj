24c30d3fad9543418e267e347581465544d224ed
fernando.avanzo <fernando.avanzo@gmail.com>
2022-04-01 16:05:03 -0300

    feat:alura-into-func-prg: remove unnessary validsation

--- src/main/clojure/course/lesson03.clj
+++ src/main/clojure/course/lesson03.clj
@@ -3,22 +3,21 @@
 ; PREDICATE
 (defn aplica-desconto?
   [valor-bruto]
-  (if (> valor-bruto 100)
-    true
-    false))
-
-(println (aplica-desconto? 000.00))
-
-(println (aplica-desconto? 100.00))
+  (> valor-bruto 100))
 
 (defn valor-descontado
   "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
   [valor-bruto]
-  (if (> valor-bruto 100)
+  (if (aplica-desconto? valor-bruto)
     (let [taxa-de-desconto (/ 10 100)
           desconto         (* valor-bruto taxa-de-desconto)]
       (- valor-bruto desconto))
     valor-bruto))
+
+;code test section
+(println (aplica-desconto? 1000.00))
+
+(println (aplica-desconto? 100.00))
 
 (println (valor-descontado 1000.00))
 
