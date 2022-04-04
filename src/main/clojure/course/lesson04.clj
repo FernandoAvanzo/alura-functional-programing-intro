7830496c42d52590e507a5090d8c698a6d14bbfd
fernando.avanzo <fernando.avanzo@gmail.com>
2022-04-04 14:31:13 -0300

    feat:alura-into-func-prg: code from lesson04.clj

--- src/main/clojure/course/lesson04.clj
+++ src/main/clojure/course/lesson04.clj
@@ -1,5 +1,5 @@
 (ns course.lesson04)
-(def precos [30 700 1000])
+(def precos [30.00 700.00 1000.00])
 
 (println "print called direct array" (precos 0))
 
@@ -31,5 +31,35 @@
 
 ;calling my function
 (println "updare arreay" (update precos 0 soma-6))
+
+;old lessons codes
+(defn aplica-desconto?
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto         (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+;using map into a array
+(println "map" (map valor-descontado precos))
+
+;using range function
+(println "Using range function" (range 10))
+
+;using filter function
+(println "filter envens with range" (filter even? (range 10)))
+
+;using filter with precos array
+(println "Original aray" precos)
+(println "filtred values" (filter aplica-desconto? precos))
+
+;apply descout in filtered pricing array
+(println "apply desxocut in filtered values" (map valor-descontado (filter aplica-desconto? precos)))
 
 
