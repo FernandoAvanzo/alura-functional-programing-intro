4b7e52b57f7992c99bb7f08e948830609e11ceab
fernando.avanzo <fernando.avanzo@gmail.com>
2022-04-11 09:28:24 -0300

    feat:alura-into-func-prg: lesson05 step02

--- src/main/clojure/course/lesson05.clj
+++ src/main/clojure/course/lesson05.clj
@@ -3,8 +3,11 @@
 (def estoque-string {"Mochile"  10
                      "Camiseta" 5})
 
-(def estoque {:Mochile  10
+(def estoque {:mochila  10
               :Camiseta 5})
+
+(def pedido {:mochila  {:quantidade 2 :preco 80}
+             :Camiseta {:quantidade 3 :preco 40}})
 
 (defn tira-um
   [valor]
@@ -26,16 +29,53 @@
 (println "orginal map" estoque)
 
 ;chnage the map value
-(println (assoc estoque :Mochile 1))
+(println (assoc estoque :mochila 1))
 
 ;using upddate function in the map
-(println "update sample" (update estoque :Mochile inc))
+(println "update sample" (update estoque :mochila inc))
 
 ;using update with my function
-(println " tira um" (update estoque :Mochile tira-um))
+(println " tira um" (update estoque :mochila tira-um))
 
 ;usind update with lambda
-(println "Lambda -3" (update estoque :Mochile #(- % 3)))
+(println "Lambda -3" (update estoque :mochila #(- % 3)))
 
 ;usinf dissoc to remove item from map
-(println "dissoc func" (dissoc estoque :Mochile ))
+(println "dissoc func" (dissoc estoque :mochila))
+
+;println enhanced maps
+(println "maps" pedido)
+
+;redefing map with a new item
+(def pedido (assoc pedido :chaveiro {:quantidade 1 :preco 10}))
+(println pedido)
+
+;accees maping as function
+(println "map intem value" (pedido :mochila))
+
+;access map item with get function
+(println "using get" (get pedido :mochila))
+
+;testing default value of get
+(println (get pedido :cadeira))
+(println (get pedido :cadeira 0))
+(println (get pedido :cadeira {}))
+
+;acessing a map value with keyword
+(println (:mochila pedido))
+
+;keyword function with default value
+(println (:cadeira pedido))
+(println (:cadeira pedido {}))
+
+;accesing a mapa inside another with keywords
+(println (:quantidade (:mochila pedido)))
+
+;icrement value item with update-in
+(println (update-in pedido [:mochila :quantidade ]  inc))
+
+;sample threading
+(-> pedido
+    :mochila
+    :quantidade
+    println)
