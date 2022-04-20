6809c2210b89541b60a2b850d308cdba5771e53d
fernando.avanzo <fernando.avanzo@gmail.com>
2022-04-20 11:12:29 -0300

    feat:alura-into-func-prg: lesson06 thread last

--- src/main/clojure/course/lesson06.clj
+++ src/main/clojure/course/lesson06.clj
@@ -5,11 +5,31 @@
              :camiseta {:quantidade 3 :preco 40}})
 
 ;create my function to understand the map interation
-(defn imprime-e-15 [valor]
-  (println "valor" (class valor) valor)
-  15)
+;function with one param of kind key value
+(defn imprime-e-15 [[chave valor]]
+  (println  chave "e"  valor)
+  valor)
+
+;calc product price
+;when i dont need of one value in the destruct i can replace the unsed value for undesrcore (_)
+(defn preco-total-do-produto [produto]
+  (* (:quantidade produto) (:preco produto)))
+
+;sum the produt values
+;using a thread last the turn the read more easy
+(defn total-do-pedido
+  [pedido]
+  (->> pedido
+       vals
+      (map preco-total-do-produto)
+      (reduce +)))
+
 
 ;test the implementation
 (println (map imprime-e-15 pedido))
+
+;prodct prices
+(println (total-do-pedido pedido))
+
 
 
