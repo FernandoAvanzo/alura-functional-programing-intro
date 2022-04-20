dd9767af92508dd5e4054ce0d08e0e05eed5fb99
fernando.avanzo <fernando.avanzo@gmail.com>
2022-04-20 12:06:27 -0300

    feat:alura-into-func-prg: finish the course

--- src/main/clojure/course/lesson06.clj
+++ src/main/clojure/course/lesson06.clj
@@ -2,7 +2,8 @@
 
 ;def map structure
 (def pedido {:mochila  {:quantidade 2 :preco 80}
-             :camiseta {:quantidade 3 :preco 40}})
+             :camiseta {:quantidade 3 :preco 40}
+             :chaveiro {:quantidade 1}})
 
 ;create my function to understand the map interation
 ;function with one param of kind key value
@@ -24,12 +25,45 @@
       (map preco-total-do-produto)
       (reduce +)))
 
+;function free iten
+;if the keyword is not finded thne return 0
+(defn gratuito? [item]
+     (<= (get item :preco 0) 0))
+
+;comp function paid
+(def pago? (comp not gratuito?))
 
 ;test the implementation
-(println (map imprime-e-15 pedido))
+;(println (map imprime-e-15 pedido))
 
 ;prodct prices
-(println (total-do-pedido pedido))
+;(println (total-do-pedido pedido))
+
+;filter itens
+(println "filtrando gratuitos")
+(println (filter gratuito? (vals pedido)))
+
+;a lot of way the return the free item
+(println (filter (fn [[chave item]] (gratuito? item)) pedido))
+(println (filter #(gratuito? (second %)) pedido))
+
+;quick test paid fuynction
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+
+;comp function sample
+(println ((comp not gratuito?) {:preco 50}))
+
+;excercises -> count total certificates
+(def clientes [
+               { :nome "Guilherme"
+                :certificados ["Clojure" "Java" "Machine Learning"] }
+               { :nome "Paulo"
+                :certificados ["Java" "Ciência da Computação"] }
+               { :nome "Daniela"
+                :certificados ["Arquitetura" "Gastronomia"] }])
+;awnser
+(println "Total certs" (->> clientes (map :certificados) (map count) (reduce +)))
 
 
 
