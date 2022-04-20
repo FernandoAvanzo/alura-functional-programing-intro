87e0c2906127716e1d6623ada6ebb4e3035bba78
fernando.avanzo <fernando.avanzo@gmail.com>
2022-04-20 10:06:39 -0300

    feat:alura-into-func-prg: lesson06 map structure analysand

--- src/main/clojure/course/lesson06.clj
+++ src/main/clojure/course/lesson06.clj
@@ -1,1 +1,15 @@
 (ns course.lesson06)
+
+;def map structure
+(def pedido {:mochila  {:quantidade 2 :preco 80}
+             :camiseta {:quantidade 3 :preco 40}})
+
+;create my function to understand the map interation
+(defn imprime-e-15 [valor]
+  (println "valor" (class valor) valor)
+  15)
+
+;test the implementation
+(println (map imprime-e-15 pedido))
+
+
